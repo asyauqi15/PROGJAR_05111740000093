c4f0c7e54178bd80bd9f321dff3e0933805bff62
Ahmad Syauqi <ahmadsyauqi1511@gmail.com>
2020-05-14 00:12:38 +0800

    tugas 9 & 10

---
+++ tugas10/runserver.sh
@@ -0,0 +1,9 @@
+#jalankan 5 async_server
+
+"C:/Users/Ahmad Syauqi/AppData/Local/Programs/Python/Python38-32/python.exe" async_server.py 9002 &
+"C:/Users/Ahmad Syauqi/AppData/Local/Programs/Python/Python38-32/python.exe" async_server.py 9003 &
+"C:/Users/Ahmad Syauqi/AppData/Local/Programs/Python/Python38-32/python.exe" async_server.py 9004 &
+"C:/Users/Ahmad Syauqi/AppData/Local/Programs/Python/Python38-32/python.exe" async_server.py 9005 &
+
+
+
